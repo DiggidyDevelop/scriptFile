c929b18fb2cd0183fdcaf52022fd2372ffc86744
Dale Grant <dalebradleygrant@yahoo.co.uk>
2019-01-03 22:09:44 +0000

    new ngrok for react

--- newScriptFile.js
+++ newScriptFile.js
@@ -7,7 +7,7 @@ prepareFrame();
 function prepareFrame() {
     console.log("yes this consoles inside of prepareFrame")
     var ifrm = document.createElement("iframe");
-    ifrm.setAttribute("src", "https://3fadb2a7.ngrok.io");
+    ifrm.setAttribute("src", "https://0a2574a2.ngrok.io/");
     ifrm.style.width = "640px";
     ifrm.style.height = "480px";
     document.body.appendChild(ifrm);
