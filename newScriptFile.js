36efa981e3421d417912dcb018cbca25970acc20
Dale Grant <dalebradleygrant@yahoo.co.uk>
2019-01-03 20:33:52 +0000

    update test console

--- newScriptFile.js
+++ newScriptFile.js
@@ -5,9 +5,12 @@ window.onload = () => {
 
 }
 alert("Hello World!");
+console.log("yes this consoles")
 prepareFrame();
 
 function prepareFrame() {
+    console.log("yes this consoles inside of prepareFrame")
+
     var ifrm = document.createElement("iframe");
     ifrm.setAttribute("src", "https://fr.wikipedia.org/wiki/Main_Page");
     ifrm.style.width = "640px";
