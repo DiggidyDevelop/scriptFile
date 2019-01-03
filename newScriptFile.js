ff9edd9294bcf7d19ea7fdafd0c8f75663060301
Dale Grant <dalebradleygrant@yahoo.co.uk>
2019-01-03 19:59:40 +0000

    new script file added

---
+++ newScriptFile.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+    prepareFrame()
+
+    alert("Hello World! onload");
+
+}
+alert("Hello World!");
+
+prepareFrame = () => {
+    var ifrm = document.createElement("iframe");
+    ifrm.setAttribute("src", "https://fr.wikipedia.org/wiki/Main_Page");
+    ifrm.style.width = "640px";
+    ifrm.style.height = "480px";
+    document.body.appendChild(ifrm);
+}
