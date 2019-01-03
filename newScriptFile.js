964d2c59ca1722060c5a5e8198885b0e4b77bdf2
Dale Grant <dalebradleygrant@yahoo.co.uk>
2019-01-03 21:59:48 +0000

    efe

--- newScriptFile.js
+++ newScriptFile.js
@@ -7,7 +7,7 @@ prepareFrame();
 function prepareFrame() {
     console.log("yes this consoles inside of prepareFrame")
     var ifrm = document.createElement("iframe");
-    ifrm.setAttribute("src", "https://6f4fb9c0.ngrok.io");
+    ifrm.setAttribute("src", "https://3fadb2a7.ngrok.io");
     ifrm.style.width = "640px";
     ifrm.style.height = "480px";
     document.body.appendChild(ifrm);
