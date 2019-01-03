025acdeab6854289a20ae4866d86a657b2ec6d94
Dale Grant <dalebradleygrant@yahoo.co.uk>
2019-01-03 20:40:08 +0000

    removed functions

--- newScriptFile.js
+++ newScriptFile.js
@@ -1,14 +1,6 @@
-window.onload = () => {
-    prepareFrame()
-
-    alert("Hello World! onload");
-
-}
 alert("Hello World!");
 console.log("yes this consoles")
-prepareFrame();
 
-function prepareFrame() {
     console.log("yes this consoles inside of prepareFrame")
 
     var ifrm = document.createElement("iframe");
@@ -16,4 +8,3 @@ function prepareFrame() {
     ifrm.style.width = "640px";
     ifrm.style.height = "480px";
     document.body.appendChild(ifrm);
-}
