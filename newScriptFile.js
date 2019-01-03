dd56e079f973994a87a67b3d7deb34c0113465fc
Dale Grant <dalebradleygrant@yahoo.co.uk>
2019-01-03 23:44:40 +0000

    iframe position

--- newScriptFile.js
+++ newScriptFile.js
@@ -10,5 +10,6 @@ function prepareFrame() {
     ifrm.setAttribute("src", "https://0a2574a2.ngrok.io/");
     ifrm.style.width = "640px";
     ifrm.style.height = "480px";
+    ifrm.style.position="relative";
     document.body.appendChild(ifrm);
 }
