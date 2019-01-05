492be741607c6f14b68b1ea5fb85c7600f32a6c8
Dale Grant <dalebradleygrant@yahoo.co.uk>
2019-01-05 17:48:42 +0000

    efef

--- newScriptFile.js
+++ newScriptFile.js
@@ -5,6 +5,7 @@ console.log("yes this consoles")
 prepareFrame();
 
 function prepareFrame() {
+    // Creating the iframe here George
     console.log("yes this consoles inside of prepareFrame")
     var ifrm = document.createElement("iframe");
     ifrm.setAttribute("src", "https://5efae1b1.ngrok.io");
@@ -15,14 +16,31 @@ function prepareFrame() {
     ifrm.style.bottom="0";
     ifrm.style.border="0";
     ifrm.setAttribute("id", "ifrm"); 
-
+// Appending the iframe to the page
     document.body.appendChild(ifrm);
 
+    //Here I tried adding an event listener for a click on the actual iframe,
+    //probably eventually quite ineffective, as even if we enlargened the iframe here
+    //We would then need to append a div in the area that the close button was
+    //To then reduce the iFrame again with another event listener, not impossible
+    //but wouldnt be effective for mobile for instance.
     document.getElementById("ifrm").contentDocument.addEventListener("click", function(event){click1();}, false);
-    iframe.document.addEventListener('click', function(event) {click1(this.id);}, false);
 
 }
+// an attempt at receiving an event from hyber-mvp client, so when the launcher
+// was accessed.. events could occur after the proper clicks of the 
+// launcher and close button.
 
+// var socket = new easyXDM.Socket({
+//     remote: "https://5efae1b1.ngrok.io", // the path to the provider
+//     onMessage:function(message, origin) {
+//     console.log('received message')   
+//  }
+// });
+
+
+//this function, connected to the event listener would have been used to increase the size of 
+//the iframe.
 function click1 () {
     console.log('colour check' + colorcheck);
 
