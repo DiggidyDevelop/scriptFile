18eed4f88173a71455a565cb5f0fdf35f41cd045
DiggidyDevelop <dalebradleygrant@yahoo.co.uk>
2019-03-10 10:31:08 +0000

    Update ZeusScript.js

--- ZeusScript.js
+++ ZeusScript.js
@@ -1,3 +1,27 @@
+function getSize() {
+    if (window.innerWidth < 450) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function alterSize() {
+    if (this.getSize()) {
+        document.getElementsByClassName('zeus-chat')[0].style.width = "100%";
+        document.getElementsByClassName('zeus-chat')[0].style.height = "100%";
+    } else {
+        document.getElementsByClassName('zeus-chat')[0].style.width = "850px";
+        document.getElementsByClassName('zeus-chat')[0].style.height = "800px";
+    }
+    console.log("Hello from function")
+}
+
+function reduceSize() {
+    document.getElementsByClassName('zeus-chat')[0].style.width = "100px";
+    document.getElementsByClassName('zeus-chat')[0].style.height = "100px";
+}
+
 window.onload = () => {
 
 
@@ -29,6 +53,17 @@ window.onload = () => {
             iframe.contentWindow.postMessage(message, "*");
             count = 1
         }
+        let receiveMessage = function(event) {
+            console.log(event.data)
+            let isOpen = JSON.parse(event.data.isOpen)
+            console.log(isOpen.isOpen, "value")
+            if (isOpen === true) {
+                this.alterSize();
+            } else if (isOpen === false) {
+                this.reduceSize()
+            }
+        }
+        window.addEventListener("message", receiveMessage, true);
     })
 
 }
