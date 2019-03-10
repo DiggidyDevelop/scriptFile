9e1699acef4dba68d10e623aee68f1d4f01366d8
DiggidyDevelop <dalebradleygrant@yahoo.co.uk>
2019-03-10 10:16:55 +0000

    Update ZeusScript.js

--- ZeusScript.js
+++ ZeusScript.js
@@ -1,64 +1,20 @@
-function getSize(){
-    if(window.innerWidth < 450){
-      return true;
-    }else{
-      return false;
+window.onload = () => {
+
+
+    function CreateIframe(){
+     return new Promise(function(resolve, reject) {
+         console.log("Inside function")
+                let iframe = document.createElement('iframe');
+                iframe.src = 'https://hyberchat.com/online/chat/';
+                iframe.className = 'zeus-chat';
+                iframe.scrolling = 'no';
+                iframe.id = "myiframe";
+                document.body.appendChild(iframe);
+            // setTimeout(function() {
+            //   resolve('foo');
+            // }, 300);
+            resolve(true)
+          });
     }
-  }
-  function alterSize(){
-    if(this.getSize()){
-      document.getElementsByClassName('zeus-chat')[0].style.width = "100%";
-      document.getElementsByClassName('zeus-chat')[0].style.height = "100%";
-    }else{
-      document.getElementsByClassName('zeus-chat')[0].style.width = "850px";
-      document.getElementsByClassName('zeus-chat')[0].style.height = "800px";
-    }
-      console.log("Hello from function")
-  }
-  function reduceSize(){
-    document.getElementsByClassName('zeus-chat')[0].style.width = "100px";
-    document.getElementsByClassName('zeus-chat')[0].style.height = "100px";
-  }
 
-
-window.onload = loadFrame;
-
-function loadFrame(){
-     var iframe = document.createElement('iframe');
-    iframe.src = 'https://hyberchat.com/online/chat/';
-    iframe.className = 'zeus-chat';
-    iframe.scrolling = 'no';
-    iframe.id = "myiframe";
-      console.log(iframe)
-    document.body.appendChild(iframe);
-   
-    
-    
-    let count = 0;
-    const message = { domain: window.location.hostname } 
-   
-    console.log(message , "Domain Name")
-    var iframe = document.getElementById('myiframe').onload = () => {
-       console.log(iframe, "iframe here")
-       console.log("message emited")
-       iframe.contentWindow.postMessage(message, "*");
-    }
- 
-         
-    let receiveMessage = function(event) {
-              console.log(event.data)
-          let isOpen = JSON.parse(event.data.isOpen)
-          console.log(isOpen.isOpen, "value")
-          if(isOpen === true){
-            this.alterSize();
-          }else if(isOpen === false){
-            this.reduceSize()
-          }
-        }
-
-           window.addEventListener("message", receiveMessage, true);
 }
- 
-   
-
-  
